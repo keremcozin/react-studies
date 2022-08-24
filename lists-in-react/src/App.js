e33317c5f82ee28a1610cde60dddb6cf96ac5ca9
keremCozin <keremCozin@pm.me>
2022-08-24 18:56:52 +0300

    Daily Study

---
+++ lists-in-react/src/App.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return <li>{item.title}</li>;
+          // item.title item.url item.author
+          // different objects, different properties
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+// The Road to React
+// Last Page: 28
