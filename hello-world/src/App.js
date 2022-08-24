bc741a6bbbf4db6dd64d35c3ffd2ff7927595c47
keremCozin <keremCozin@pm.me>
2022-08-24 17:52:13 +0300

    Hello World

---
+++ hello-world/src/App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+const welcome = {
+  greeting: 'Hello',
+  title: 'World',
+};
+
+function App() {
+  return (
+    <div>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
